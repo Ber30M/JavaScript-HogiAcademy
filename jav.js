b90d8a5c01b75a041e051ea7781a79963ef10f3b
Ber30 <makoberobertrand@gmail.com>
2022-07-22 15:50:46 +0200

    'init'

---
+++ jav.js
@@ -0,0 +1,14 @@
+// //dom
+// document.addEventListener('click', (bouton) => {
+//     const valeur = bouton.target.dataset.key;
+//     calculer(valeur)
+// })
+// const touches = [...document.querySelectorAll('.bouton')];
+// console.log('touches');
+
+// const listeKeycode = touches.map(touche => touches.dataset.key);
+
+// document.addEventListener('click', (e) => {
+//     const valeur = e.target;
+//     ecran.textContent = "";
+// })
