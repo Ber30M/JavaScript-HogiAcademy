c79bf12cb1f575cb49410e150e6d017c024b1888
Ber30 <makoberobertrand@gmail.com>
2022-07-27 01:36:33 +0200

    none ko bitajako

--- jav.js
+++ jav.js
@@ -12,3 +12,32 @@
 //     const valeur = e.target;
 //     ecran.textContent = "";
 // })
+        // Button zose onClick zihe fonction imwe en paramètre ironka event.
+
+        // Hama champ ijamwo ivyo banditse uyihe id
+
+        // Muri fonction, ligne ya mbere ni 
+
+        // var champ = getElementById("...")
+
+        // ligne ya 2 ni
+
+        // champ.value=event.target.innerText
+
+
+//THE FUNCTIONNING CODE
+
+
+function fonctionYoKwandika(x){
+        document.getElementById("ecran").innerHTML +=x
+};
+function fonctionYoGufuta(){
+        document.getElementById("ecran").innerHTML= ""
+}
+
+
+function fonctionYoGuharura(){
+let inyishu = document.getElementById("ecran");
+inyishu.innerText = eval(inyishu.innerText);
+
+}
